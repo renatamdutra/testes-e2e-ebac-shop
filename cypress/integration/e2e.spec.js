1d7357457aa341e51497d2133e4b8b6328176585
Renata Dutra <renatamachadodutra@gmail.com>
2022-11-05 16:19:09 -0300

    Exercício refeito

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,21 +1,73 @@
 /// <reference types="cypress" />
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
+    /*  Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
-        cy.visit('/')
+     it('deve adicionar produtos ao carrinho e finalizar compra', () => {
+
+        cy.visit('produtos')
+        cy.get(':nth-child(4) > .page-numbers').click()
+
+        cy.get('[class="product-block grid"]')
+            .contains('Balboa Persistence Tee').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
+
+        cy.visit('produtos')
+        cy.get(':nth-child(4) > .page-numbers').click()
+
+        cy.get('[class="product-block grid"]')
+            .contains('Autumn Pullie').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+
+        cy.visit('produtos')
+        cy.get(':nth-child(4) > .page-numbers').click()
+
+        cy.get('[class="product-block grid"]')
+            .contains('Bruno Compete Hoodie').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 3)
+
+        cy.visit('produtos')
+        cy.get(':nth-child(4) > .page-numbers').click()
+
+        cy.get('[class="product-block grid"]')
+            .contains('Cassia Funnel Sweatshirt').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Purple').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
+        
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+
+        cy.get('#billing_first_name').type('Renata')
+        cy.get('#billing_last_name').type('Machado')
+        cy.get('#billing_address_1').type('Ernesto ferreira')
+        cy.get('#billing_city').type('Lagoa')
+        cy.get('#billing_postcode').type('95300000')
+        cy.get('#billing_phone').type('54999343224')
+        cy.get('#billing_email').type('teste@teste.com')
+
+        cy.get('#terms').click()
+
+        cy.get('#place_order').click()
+
+
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
-
-
-})
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,13 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutos', (produto, tamanho, quantidade) =>{
+    cy.get('[class="product-block grid"]')
+        .contains(produto).click()
+    cy.get('.button-variable-item-'+ tamanho).click()
+    cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+
+
+})
